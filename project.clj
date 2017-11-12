32fb9e8876c1bd762724de9f3d3bbd2687766cc8
esteban <esteban.leon@intertecintl.com>
2017-11-11 23:51:00 -0600

    Added project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject text-utils "0.1.0"
   :description "Utility functions to work with text data"
-  :url ""
+  :url "https://github.com/estebanleonsoto/text-utils"
   :license {:name "GNU public licence V3.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
